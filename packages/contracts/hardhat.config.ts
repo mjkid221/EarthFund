fe729a45e789af3691b00f308204a88d8659eb4a
justin-labrys <justin@labrys.io>
2022-03-21 09:41:21 +1000

    feat: added gas reporter

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -25,17 +25,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.13",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    hardhat: {},
+    localhost: {},
+    goerli: {
+      url: process.env.GOERLI_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
+  namedAccounts: {
+    deployer: 0,
+  },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP || "",
+    currency: "AUD",
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
