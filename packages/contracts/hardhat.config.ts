de0e8b0fcc030d789589ca178ca3375edd933a9d
justin-labrys <justin@labrys.io>
2022-03-21 11:15:14 +1000

    feat: hardhat works

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,51 +1,17 @@
-import * as dotenv from "dotenv";
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/types";
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-ethers";
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
 import "hardhat-gas-reporter";
+import "@typechain/hardhat";
 import "solidity-coverage";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 
-dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-const config: HardhatUserConfig = {
+module.exports = {
   solidity: "0.8.13",
   networks: {
     hardhat: {},
-    localhost: {},
-    goerli: {
-      url: process.env.GOERLI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  namedAccounts: {
-    deployer: 0,
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    coinmarketcap: process.env.COINMARKETCAP || "",
-    currency: "AUD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
-
-export default config;
