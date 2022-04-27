77d2aae6020196a8ecdbd189875082d200822b26
Steven Balagtas <steven@labrys.io>
2022-04-27 14:21:24 +1000

    feat: updated comments in the clearing house contract interface

--- packages/contracts/contracts/interfaces/IClearingHouse.sol
+++ packages/contracts/contracts/interfaces/IClearingHouse.sol
@@ -12,17 +12,33 @@ interface IClearingHouse {
                 FUNCTIONS
     ///////////////////////////////////*/
 
+  /**
+   * @notice Adds a child dao token to the register of swappable tokens
+   * @param _childDaoToken The address of the child dao's ERC20 token contract
+   */
   function registerChildDao(address _childDaoToken) external;
 
+  /**
+   * @notice Swaps a user's 1Earth tokens for a specific child dao's tokens
+   * @param _childDaoToken The address of the child dao's ERC20 token contract
+   * @param _account The address of the user whose tokens are being swapped
+   * @param _amount The amount of 1Earth tokens being swapped
+   */
   function swapForChildDaoTokens(
     address _childDaoToken,
     address _account,
-    uint256 amount
+    uint256 _amount
   ) external;
 
+  /**
+   * @notice Swaps a user's tokens for a specific child dao for 1Earth tokens
+   * @param _childDaoToken The address of the child dao's ERC20 token contract
+   * @param _account The address of the user whose tokens are being swapped
+   * @param _amount The amount of child dao tokens being swapped
+   */
   function swapForEarthTokens(
     address _childDaoToken,
     address _account,
-    uint256 amount
+    uint256 _amount
   ) external;
 }
