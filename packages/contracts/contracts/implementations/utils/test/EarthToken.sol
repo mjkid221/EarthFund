63cc085c17f45e7f0a47da1281f77f461d1e83e3
Steven Balagtas <steven@labrys.io>
2022-05-05 15:08:31 +1000

    fix: removed hardhat console log from the earth token test contract

--- packages/contracts/contracts/implementations/utils/test/EarthToken.sol
+++ packages/contracts/contracts/implementations/utils/test/EarthToken.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
 
 contract EarthToken is ERC20 {
   constructor(uint256 _supply) ERC20("One Earth", "1EARTH") {
