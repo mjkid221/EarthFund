d9b00c94fbc126a5aba7943002829bd09e4e5c91
justin-labrys <justin@labrys.io>
2022-06-08 13:27:56 +1000

    feat: added environment checks

--- packages/contracts/deploy/002_deploy_clearing_house.ts
+++ packages/contracts/deploy/002_deploy_clearing_house.ts
@@ -6,15 +6,27 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = await hre.getChainId();
 
   const earthToken = await ethers.getContract("EarthToken");
+
+  let maxSupply = process.env.MAX_SUPPLY;
+  let maxSwap = process.env.MAX_SWAP;
+  if (chainId == "31337") {
+    maxSupply = ethers.utils.parseEther("1000000").toString();
+    maxSwap = ethers.utils.parseEther("5000").toString();
+  }
+
+  if (!maxSupply || !maxSwap) {
+    throw new Error("Limit parameters not found");
+  }
 
   await deploy("ClearingHouse", {
     from: deployer,
     args: [
       process.env.EARTH_ERC20_TOKEN_ADDRESS ?? earthToken.address,
-      ethers.utils.parseEther("1000000"),
-      ethers.utils.parseEther("5000"),
+      maxSupply,
+      maxSwap,
     ],
     log: true,
   });
