d8992ddc8e329ffbc4d454c6521e20e441de2cad
Hamish McLean <hamish@labrys.io>
2022-06-14 10:46:05 +1000

    🔨 scripts: add basic deploy scripts

--- packages/contracts/deploy/006_deploy_thin_wallet.ts
+++ packages/contracts/deploy/006_deploy_thin_wallet.ts
@@ -6,12 +6,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("EarthToken", {
+  await deploy("ThinWallet", {
     from: deployer,
-    args: [ethers.constants.MaxUint256],
+    args: [],
     log: true,
   });
+
+  const ThinWallet = await ethers.getContract("ThinWallet");
+
+  await ThinWallet.initialize(deployer, []);
 };
 
 export default func;
-func.tags = ["testbed", "_EarthToken", "_ThinWallet"];
+func.tags = ["testbed", "_ThinWallet"];
