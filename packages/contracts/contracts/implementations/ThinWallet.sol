0b013ec18b17066763be88dcaee30f90bc552061
Hamish McLean <hamish@labrys.io>
2022-06-14 11:00:43 +1000

    💡 comments: update contract comments

--- packages/contracts/contracts/implementations/ThinWallet.sol
+++ packages/contracts/contracts/implementations/ThinWallet.sol
@@ -10,14 +10,14 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "../interfaces/IThinWallet.sol";
 
 contract ThinWallet is IThinWallet, Initializable, AccessControl {
+  // Access Control Role Definitions
   bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER");
   bytes32 public constant TRANSFER_ADMIN_ROLE = keccak256("TRANSFER_ADMIN");
+
+  // Store admin address
   address public admin;
 
   /// ### Functions
-  /// @notice Initializes the thin wallet clone with the accounts that can control it
-  /// @param _admin  This is should be set as the default admin. This will be the donation router
-  /// @param _owners  The accounts that can call the transfer functions
   function initialize(address _admin, address[] calldata _owners) external {
     require(_admin != address(0), "admin address cannot be 0x0");
     admin = _admin;
@@ -29,10 +29,6 @@ contract ThinWallet is IThinWallet, Initializable, AccessControl {
     }
   }
 
-  /// @notice Transfers amounts of an ERC20 to one or more recipients
-  /// @dev If the `setApprove` field is true, the contract should approve that recipient for type(uint256).max
-  /// @param _transfers  An array of transfers. Each transfer object specifies the amount and recipient to send tokens to
-  /// @param _approvals  An array of approval objects. Each approval specifies the recipient and the amount to approve that recipient to spend.
   function transferERC20(
     TokenMovement[] calldata _transfers,
     TokenMovement[] calldata _approvals
@@ -52,9 +48,6 @@ contract ThinWallet is IThinWallet, Initializable, AccessControl {
     }
   }
 
-  /// @notice Transfers amounts of ether to one or more recipeints
-  /// @dev This should use address(recipient).call to transfer the ether
-  /// @param _transfers  The ether transfers
   function transferEther(EtherPaymentTransfer[] calldata _transfers) external {
     require(
       hasRole(TRANSFER_ROLE, msg.sender) || msg.sender == admin,
