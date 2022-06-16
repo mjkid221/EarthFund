9a55c2540b746bc4308b9ec73f2d04df5ca21896
mjLabrys <mj@labrys.io>
2022-06-16 11:51:11 +1000

    refactored tests

--- packages/contracts/test/ThinWallet.test.ts
+++ packages/contracts/test/ThinWallet.test.ts
@@ -24,10 +24,11 @@ describe("Thin Wallet", async () => {
     ThinWallet: ThinWallet,
     deployer: SignerWithAddress,
     userA : SignerWithAddress,
-    userB : SignerWithAddress;
+    userB : SignerWithAddress,
+    userC : SignerWithAddress;
 
   beforeEach(async () => {
-    [deployer, userA, userB] = await ethers.getSigners();
+    [deployer, userA, userB, userC] = await ethers.getSigners();
     await deployments.fixture(["_EarthToken"]);
     EarthToken = await ethers.getContract("EarthToken");
 
@@ -36,15 +37,16 @@ describe("Thin Wallet", async () => {
       log: false,
       args: [],
     });
-    
-    ThinWallet = await ethers.getContract("ThinWallet");
-    await ThinWallet.initialize(deployer.address, [
-      userA.address,
-    ]);
-    
   });
 
   describe("Initialize Thin Wallet", async () => {
+    beforeEach(async () => {
+      ThinWallet = await ethers.getContract("ThinWallet");
+
+      await ThinWallet.initialize(deployer.address, [
+        userA.address,
+      ]);
+    });
 
     it("should not deploy with zero address admin", async () => {
       await expect(
@@ -60,6 +62,14 @@ describe("Thin Wallet", async () => {
   });
 
   describe("Thin Wallet Contract : RECEIVE function", async () => {
+    beforeEach(async () => {
+      ThinWallet = await ethers.getContract("ThinWallet");
+
+      await ThinWallet.initialize(deployer.address, [
+        userA.address,
+      ]);
+    });
+
     it("should properly receieve ether", async () => {
   
       const amountToTransfer : BigNumber = (await ethers.provider.getBalance(deployer.address)).div(2);
@@ -75,36 +85,40 @@ describe("Thin Wallet", async () => {
   });
 
   describe("Thin Wallet Access Control", async () => {
-    it("should give admin TRANSFER_ADMIN role", async () => {
+    beforeEach(async () => {
+      ThinWallet = await ethers.getContract("ThinWallet");
+
       await ThinWallet.initialize(deployer.address, [
         userA.address,
       ]);
+    });
+
+    it("should give admin TRANSFER_ADMIN role", async () => {
       await expect(ThinWallet.hasRole(TRANSFER_ADMIN, deployer.address));
     });
 
     it("should give admin DEFAULT_TRANSFER role", async () => {
-      await ThinWallet.initialize(deployer.address, [
-        userA.address,
-      ]);
       await expect(ThinWallet.hasRole(DEFAULT_ADMIN_ROLE, deployer.address));
     });
 
     it("should give owners TRANSFER_ROLE role", async () => {
-      await ThinWallet.initialize(deployer.address, [
-        userA.address,
-      ]);
       await expect(ThinWallet.hasRole(TRANSFER, userA.address));
     })
 
     it("should give owners TRANSFER_ADMIN_ROLE role", async () => {
-      await ThinWallet.initialize(deployer.address, [
-        userA.address,
-      ]);
       await expect(ThinWallet.hasRole(TRANSFER_ADMIN, userA.address));
     })
   });
 
   describe("Thin Wallet Transfer : Success cases", async () => {
+    beforeEach(async () => {
+      ThinWallet = await ethers.getContract("ThinWallet");
+
+      await ThinWallet.initialize(deployer.address, [
+        userA.address,
+      ]);
+    });
+
     it("should transfer ERC20 token successfully by admin", async () => {
       expect (await EarthToken.balanceOf(userB.address)).to.be.eq(0);
 
@@ -180,14 +194,14 @@ describe("Thin Wallet", async () => {
     });
 
     it("should handle multiple ERC20 token transfers", async () => {
-      expect (await EarthToken.balanceOf(userA.address)).to.be.eq(0);
+      expect (await EarthToken.balanceOf(userC.address)).to.be.eq(0);
       expect (await EarthToken.balanceOf(userB.address)).to.be.eq(0);
 
       // Send some EarthToken to contract for testing
       const amountToTransfer : BigNumber = ethers.utils.parseEther("1000000")
       await EarthToken.connect(deployer).transfer(ThinWallet.address, amountToTransfer)
       
-      const userAInitialBalance : BigNumber = await EarthToken.balanceOf(userA.address);
+      const userCInitialBalance : BigNumber = await EarthToken.balanceOf(userC.address);
       const userBInitialBalance : BigNumber = await EarthToken.balanceOf(userB.address);
 
       let tokenTransfers : TokenMovement[] = [];
@@ -203,23 +217,23 @@ describe("Thin Wallet", async () => {
         }else{
           tokenTransfer = {
             token: EarthToken.address,
-            recipient: userA.address,
+            recipient: userC.address,
             amount: amountToTransfer.div(4)
           };
           tokenTransfers.push(tokenTransfer);
         }
       }
       await ThinWallet.transferERC20(tokenTransfers);
-      expect (await EarthToken.balanceOf(userA.address)).to.be.eq(userAInitialBalance.add(amountToTransfer.div(2)));
+      expect (await EarthToken.balanceOf(userC.address)).to.be.eq(userCInitialBalance.add(amountToTransfer.div(2)));
       expect (await EarthToken.balanceOf(userB.address)).to.be.eq(userBInitialBalance.add(amountToTransfer.div(2)));
     });
 
     it("should handle multiple ether transfers", async () => {
-      expect (await ethers.provider.getBalance(userA.address)).to.be.eq(ethers.utils.parseEther("10000"));
+      expect (await ethers.provider.getBalance(userC.address)).to.be.eq(ethers.utils.parseEther("10000"));
       expect (await ethers.provider.getBalance(userB.address)).to.be.eq(ethers.utils.parseEther("10000"));
 
       const amountToTransfer : BigNumber = ethers.utils.parseEther("1000");
-      const userAInitialBalance : BigNumber = await ethers.provider.getBalance(userA.address);
+      const userCInitialBalance : BigNumber = await ethers.provider.getBalance(userC.address);
       const userBInitialBalance : BigNumber = await ethers.provider.getBalance(userB.address);
   
       await deployer.sendTransaction({
@@ -238,7 +252,7 @@ describe("Thin Wallet", async () => {
           etherTransfers.push(etherTransfer);
         }else{
           etherTransfer = {
-            recipient: userA.address,
+            recipient: userC.address,
             amount: amountToTransfer.div(4)
           };
           etherTransfers.push(etherTransfer);
@@ -247,12 +261,20 @@ describe("Thin Wallet", async () => {
     
       await ThinWallet.transferEther(etherTransfers);
   
-      expect (await ethers.provider.getBalance(userA.address)).to.be.eq(userAInitialBalance.add(amountToTransfer.div(2)));
+      expect (await ethers.provider.getBalance(userC.address)).to.be.eq(userCInitialBalance.add(amountToTransfer.div(2)));
       expect (await ethers.provider.getBalance(userB.address)).to.be.eq(userBInitialBalance.add(amountToTransfer.div(2)));
     });
   });
 
   describe("Thin Wallet Transfer : Fail cases", async () => {
+    beforeEach(async () => {
+      ThinWallet = await ethers.getContract("ThinWallet");
+
+      await ThinWallet.initialize(deployer.address, [
+        userA.address,
+      ]);
+    });
+
     it("should fail to transfer ERC20 token from call by non-admin or non-owner", async () => {
       expect (await EarthToken.balanceOf(userB.address)).to.be.eq(0);
 
@@ -268,9 +290,10 @@ describe("Thin Wallet", async () => {
 
       const expectedErrorMsg = `InvalidPermissions("${userB.address}")`;
       await expect (ThinWallet.connect(userB).transferERC20([tokenTransfer])).to.be.revertedWith(expectedErrorMsg);
+      expect (await EarthToken.balanceOf(userB.address)).to.be.eq(0);
     });
     it("should fail to transfer ether from call by non-admin or non-owner ", async () => {
-      expect (await ethers.provider.getBalance(userB.address)).to.be.eq(ethers.utils.parseEther("10000"));
+      expect (await ethers.provider.getBalance(userC.address)).to.be.eq(ethers.utils.parseEther("10000"));
   
       const amountToTransfer : BigNumber = (await ethers.provider.getBalance(deployer.address)).div(2);
   
@@ -286,10 +309,11 @@ describe("Thin Wallet", async () => {
       
       const expectedErrorMsg = `InvalidPermissions("${userB.address}")`;
       await expect(ThinWallet.connect(userB).transferEther([EtherTransfer])).to.be.revertedWith(expectedErrorMsg);
+      expect (await ethers.provider.getBalance(userC.address)).to.be.eq(ethers.utils.parseEther("10000"));
     });
 
     it("should fail to transfer ether when the contract has not enough ether balance", async () => {
-      expect (await ethers.provider.getBalance(userA.address)).to.be.eq(ethers.utils.parseEther("10000"));
+      expect (await ethers.provider.getBalance(userB.address)).to.be.eq(ethers.utils.parseEther("10000"));
   
       const amountToTransfer : BigNumber = (await ethers.provider.getBalance(deployer.address)).div(2);
   
@@ -305,10 +329,10 @@ describe("Thin Wallet", async () => {
       
       const expectedErrorMsg = `failed to send ether`;
       await expect(ThinWallet.connect(userA).transferEther([EtherTransfer])).to.be.revertedWith(expectedErrorMsg);
-    });
+      expect (await ethers.provider.getBalance(userB.address)).to.be.eq(ethers.utils.parseEther("10000"));    });
 
     it("should fail to transfer token when the contract has not enough token balance", async () => {
-      expect (await EarthToken.balanceOf(userA.address)).to.be.eq(0);
+      expect (await EarthToken.balanceOf(userB.address)).to.be.eq(0);
 
       // Send some EarthToken to contract for testing
       const amountToTransfer : BigNumber = ethers.utils.parseEther("1000"); 
@@ -322,6 +346,7 @@ describe("Thin Wallet", async () => {
       };
 
       await expect (ThinWallet.connect(userA).transferERC20([tokenTransfer])).to.be.reverted;
+      expect (await EarthToken.balanceOf(userB.address)).to.be.eq(0);
     });
   });
 });
