3468f4c4a20376ac4a33bcaaddd211c9052b94eb
mjLabrys <mj@labrys.io>
2022-06-21 16:00:52 +1000

    Added test staking reward token for testing on rinkeby

---
+++ packages/contracts/contracts/implementations/utils/test/TestStakingRewardToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StakingRewardToken is ERC20 {
+    constructor(uint256 _supply) ERC20("Test Tether USD", "tUSDT") {
+        _mint(msg.sender, _supply); // mint total supply the deployer account
+    }
+}
