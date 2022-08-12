f5def800dbd4e058b2b9e1dfbcc460049bfb9530
Hamish McLean <hamish@labrys.io>
2022-08-12 11:22:16 +1000

    ✨ feat: create first draft of donation proxy

--- packages/contracts/contracts/implementations/DonationProxy.sol
+++ packages/contracts/contracts/implementations/DonationProxy.sol
@@ -2,13 +2,27 @@
 pragma solidity 0.8.13;
 
 import "../interfaces/IDonationsProxy.sol";
+import "../interfaces/IThinWallet.sol";
 
 contract DonationProxy is IDonationsProxy {
+  ERC20 public immutable baseToken;
+
+  constructor(ERC20 _baseToken) {
+    baseToken = _baseToken;
+  }
+
   function deposit(
     uint256 amount,
     ERC20 token,
+    IThinWallet target,
     bytes calldata swapCallData
-  ) external {}
+  ) external payable override {
+    (bool success, ) = address(baseToken).call{ value: msg.value }(
+      swapCallData
+    );
+    require(success, "swap failed");
+    emit SwapDeposit(msg.sender, amount, token, target);
+  }
 
   receive() external payable {}
 }
--- packages/contracts/contracts/interfaces/IDonationsProxy.sol
+++ packages/contracts/contracts/interfaces/IDonationsProxy.sol
@@ -2,11 +2,34 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IThinWallet.sol";
 
 interface IDonationsProxy {
+  /**
+    @notice emits when a user swaps a token to deposit into a thin wallet
+    @param user the user who swapped the token
+    @param amount the amount the user swapped of their token
+    @param token the token that the user swapped
+    @param target the thin wallet that the user was depositing into
+  */
+  event SwapDeposit(
+    address user,
+    uint256 amount,
+    ERC20 token,
+    IThinWallet target
+  );
+
+  /**
+    @notice this function is used for depositing tokens to be swapped and donated
+    @param amount the amount of token that will be swapped and donated
+    @param token the token that will be swapped
+    @param target the thin wallet that will be donated to
+    @param swapCallData the call data supplied by 0x that will be used to swap the token
+  */
   function deposit(
     uint256 amount,
     ERC20 token,
+    IThinWallet target,
     bytes calldata swapCallData
-  ) external;
+  ) external payable;
 }
