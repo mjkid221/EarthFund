6b38013853a01ed565d25019286d3e897237d319
Hamish McLean <hamish@labrys.io>
2022-08-12 10:35:28 +1000

    🚧 wip: get basic structure down

--- packages/contracts/contracts/interfaces/IDonationsProxy.sol
+++ packages/contracts/contracts/interfaces/IDonationsProxy.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../interfaces/IDonationsProxy.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DonationProxy is IDonationsProxy {
+interface IDonationsProxy {
   function deposit(
     uint256 amount,
     ERC20 token,
     bytes calldata swapCallData
-  ) external {}
-
-  receive() external payable {}
+  ) external;
 }
