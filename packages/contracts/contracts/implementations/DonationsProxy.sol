76670dd15a7900051b9f682d434bd1d6d7c0030d
Hamish McLean <hamish@labrys.io>
2022-08-16 10:10:18 +1000

    🚧 wip: update draft donations proxy

--- packages/contracts/contracts/implementations/DonationsProxy.sol
+++ packages/contracts/contracts/implementations/DonationsProxy.sol
@@ -4,11 +4,24 @@ pragma solidity 0.8.13;
 import "../interfaces/IDonationsProxy.sol";
 import "../interfaces/IThinWallet.sol";
 
-contract DonationProxy is IDonationsProxy {
+contract DonationsProxy is IDonationsProxy {
   ERC20 public immutable baseToken;
 
   constructor(ERC20 _baseToken) {
     baseToken = _baseToken;
+  }
+
+  function depositETH(
+    uint256 amount,
+    IThinWallet target,
+    bytes calldata swapCallData
+  ) external payable override {
+    require(msg.value > amount, "insufficient amount supplied");
+    (bool success, ) = address(baseToken).call{ value: (msg.value - amount) }(
+      swapCallData
+    );
+    require(success, "swap failed");
+    emit SwapETH(msg.sender, msg.value, target);
   }
 
   function deposit(
--- packages/contracts/contracts/interfaces/IDonationsProxy.sol
+++ packages/contracts/contracts/interfaces/IDonationsProxy.sol
@@ -20,6 +20,14 @@ interface IDonationsProxy {
   );
 
   /**
+    @notice emits when a user swaps a token to deposit into a thin wallet
+    @param user the user who swapped the token
+    @param amount the amount the user swapped of their token
+    @param target the thin wallet that the user was depositing into
+  */
+  event SwapETH(address user, uint256 amount, IThinWallet target);
+
+  /**
     @notice this function is used for depositing tokens to be swapped and donated
     @param amount the amount of token that will be swapped and donated
     @param token the token that will be swapped
@@ -29,6 +37,18 @@ interface IDonationsProxy {
   function deposit(
     uint256 amount,
     ERC20 token,
+    IThinWallet target,
+    bytes calldata swapCallData
+  ) external payable;
+
+  /**
+    @notice this function is used for depositing tokens to be swapped and donated
+    @param amount the amount of token that will be swapped and donated
+    @param target the thin wallet that will be donated to
+    @param swapCallData the call data supplied by 0x that will be used to swap the token
+  */
+  function depositETH(
+    uint256 amount,
     IThinWallet target,
     bytes calldata swapCallData
   ) external payable;
