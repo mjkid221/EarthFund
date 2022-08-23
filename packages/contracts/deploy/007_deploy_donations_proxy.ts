0de7061aaf0b98ff3bb8030bb640e6fb3968c3ec
Hamish McLean <hamish@labrys.io>
2022-08-24 09:25:09 +1000

    🔨 scripts: add donations proxy deploy script

---
+++ packages/contracts/deploy/007_deploy_donations_proxy.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import "dotenv/config";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const USDT_ADDRESS = process.env.USDT_ADDRESS;
+  const WETH_ADDRESS = process.env.WETH_ADDRESS;
+
+  await deploy("DonationsProxy", {
+    from: deployer,
+    args: [WETH_ADDRESS, USDT_ADDRESS],
+    log: true,
+  });
+};
+
+export default func;
+func.tags = ["_DonationsProxy"];
