3fe329b7933cc85f34afdef97fc5621803225204
Hamish McLean <hamish@labrys.io>
2022-08-24 09:24:00 +1000

    🐛 fix: 0x swap failed bugfix

--- packages/contracts/contracts/implementations/DonationsProxy.sol
+++ packages/contracts/contracts/implementations/DonationsProxy.sol
@@ -1,41 +1,74 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import "../interfaces/IDonationsProxy.sol";
-import "../interfaces/IThinWallet.sol";
+
+interface IWETH is IERC20 {
+  function deposit() external payable;
+}
 
 contract DonationsProxy is IDonationsProxy {
-  ERC20 public immutable baseToken;
+  using SafeERC20 for ERC20;
+  using SafeERC20 for IWETH;
 
-  constructor(ERC20 _baseToken) {
-    baseToken = _baseToken;
-  }
+  // The Targeted ERC20 for Swaps
+  ERC20 public baseToken;
 
-  function depositETH(
-    uint256 amount,
-    IThinWallet target,
-    bytes calldata swapCallData
-  ) external payable override {
-    require(msg.value > amount, "insufficient amount supplied");
-    (bool success, ) = address(baseToken).call{ value: (msg.value - amount) }(
-      swapCallData
-    );
-    require(success, "swap failed");
-    emit SwapETH(msg.sender, msg.value, target);
-  }
+  // The WETH contract.
+  IWETH public immutable WETH;
 
-  function deposit(
-    uint256 amount,
-    ERC20 token,
-    IThinWallet target,
-    bytes calldata swapCallData
-  ) external payable override {
-    (bool success, ) = address(baseToken).call{ value: msg.value }(
-      swapCallData
-    );
-    require(success, "swap failed");
-    emit SwapDeposit(msg.sender, amount, token, target);
+  constructor(IWETH _weth, IERC20 _baseToken) {
+    WETH = _weth;
+    _baseToken = _baseToken;
   }
 
   receive() external payable {}
+
+  function depositETH(
+    ERC20 buyToken,
+    uint256 amount,
+    address location,
+    address spender,
+    address payable swapTarget,
+    bytes calldata swapCallData
+  ) external payable override {
+    uint256 boughtAmount = buyToken.balanceOf(address(this));
+    WETH.deposit{ value: amount }();
+    if (WETH.allowance(address(this), spender) < amount) {
+      WETH.safeApprove(spender, type(uint256).max);
+    }
+    (bool success, ) = swapTarget.call{ value: msg.value - amount }(
+      swapCallData
+    );
+    require(success, "SWAP_CALL_FAILED");
+    payable(msg.sender).transfer(address(this).balance);
+    boughtAmount = buyToken.balanceOf(address(this)) - boughtAmount;
+    emit SwapETH(msg.sender, amount, location);
+    buyToken.safeTransfer(location, boughtAmount);
+  }
+
+  function depositERC20(
+    ERC20 sellToken,
+    ERC20 buyToken,
+    uint256 amount,
+    address location,
+    address spender,
+    address payable swapTarget,
+    bytes calldata swapCallData
+  ) external payable override {
+    sellToken.safeTransferFrom(msg.sender, address(this), amount);
+    uint256 boughtAmount = buyToken.balanceOf(address(this));
+    if (sellToken.allowance(address(this), spender) < amount) {
+      sellToken.safeApprove(spender, type(uint256).max);
+    }
+    (bool success, ) = swapTarget.call{ value: msg.value }(swapCallData);
+    require(success, "SWAP_CALL_FAILED");
+    payable(msg.sender).transfer(address(this).balance);
+    boughtAmount = buyToken.balanceOf(address(this)) - boughtAmount;
+    emit SwapDeposit(msg.sender, amount, sellToken, location);
+    buyToken.safeTransfer(location, boughtAmount);
+  }
 }
--- packages/contracts/contracts/interfaces/IDonationsProxy.sol
+++ packages/contracts/contracts/interfaces/IDonationsProxy.sol
@@ -12,12 +12,7 @@ interface IDonationsProxy {
     @param token the token that the user swapped
     @param target the thin wallet that the user was depositing into
   */
-  event SwapDeposit(
-    address user,
-    uint256 amount,
-    ERC20 token,
-    IThinWallet target
-  );
+  event SwapDeposit(address user, uint256 amount, ERC20 token, address target);
 
   /**
     @notice emits when a user swaps a token to deposit into a thin wallet
@@ -25,31 +20,43 @@ interface IDonationsProxy {
     @param amount the amount the user swapped of their token
     @param target the thin wallet that the user was depositing into
   */
-  event SwapETH(address user, uint256 amount, IThinWallet target);
+  event SwapETH(address user, uint256 amount, address target);
 
   /**
-    @notice this function is used for depositing tokens to be swapped and donated
-    @param amount the amount of token that will be swapped and donated
-    @param token the token that will be swapped
-    @param target the thin wallet that will be donated to
-    @param swapCallData the call data supplied by 0x that will be used to swap the token
+    @notice this function is used to swap donated ETH to a desired token before it is deposited into another external contract
+    @param buyToken the token that the user wishes to swap for, should be the base token
+    @param amount the amount the user is selling of their sellToken
+    @param location where the bought tokens will be sent after they are swapped
+    @param spender an address provided by the 0x Quote API
+    @param swapTarget an address provided by the 0x Quote API
+    @param swapCallData the calldata provided by the 0x Quote API
   */
-  function deposit(
+  function depositETH(
+    ERC20 buyToken,
     uint256 amount,
-    ERC20 token,
-    IThinWallet target,
+    address location,
+    address spender,
+    address payable swapTarget,
     bytes calldata swapCallData
   ) external payable;
 
   /**
-    @notice this function is used for depositing tokens to be swapped and donated
-    @param amount the amount of token that will be swapped and donated
-    @param target the thin wallet that will be donated to
-    @param swapCallData the call data supplied by 0x that will be used to swap the token
+    @notice this function is used to swap donated ERC20 tokens to a desired token before it is deposited into another external contract
+    @param sellToken the token that the user is selling to acquire the base token
+    @param buyToken the token that the user wishes to swap for, should be the base token
+    @param amount the amount the user is selling of their sellToken
+    @param location where the bought tokens will be sent after they are swapped
+    @param spender an address provided by the 0x Quote API
+    @param swapTarget an address provided by the 0x Quote API
+    @param swapCallData the calldata provided by the 0x Quote API
   */
-  function depositETH(
+  function depositERC20(
+    ERC20 sellToken,
+    ERC20 buyToken,
     uint256 amount,
-    IThinWallet target,
+    address location,
+    address spender,
+    address payable swapTarget,
     bytes calldata swapCallData
   ) external payable;
 }
